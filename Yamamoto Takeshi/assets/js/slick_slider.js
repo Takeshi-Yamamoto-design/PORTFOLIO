a4346e82021090e6835db9826443cc291e5eef1f
Takeshi-Yamamoto-design <yamatakeguard@icloud.com>
2026-07-15 21:09:54 +0900

    Add files via upload

---
+++ Yamamoto Takeshi/assets/js/slick_slider.js
@@ -0,0 +1,9 @@
+$(function () {
+  $('.slider').slick({
+    arrows: false,
+    dots: true,
+    autoplay: false,
+    swipe: true,        // フリックON
+    draggable: true,    // PCドラッグもON
+  });
+});
