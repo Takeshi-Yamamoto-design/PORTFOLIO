4e380febf953dc6d8a4675d3907076111cafd63a
Takeshi-Yamamoto-design <yamatakeguard@icloud.com>
2026-07-15 21:40:39 +0900

    Delete Yamamoto Takeshi directory

--- Yamamoto Takeshi/assets/js/slick_slider.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $('.slider').slick({
-    arrows: false,
-    dots: true,
-    autoplay: false,
-    swipe: true,        // フリックON
-    draggable: true,    // PCドラッグもON
-  });
-});
